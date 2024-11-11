38dafe2eeda797d07c6c1b123d2dbc3a85e0f7db
Skeletron01 <benjaminbutler2007@gmail.com>
2024-11-11 12:58:36 +1030

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,25 @@
 calculations = [] #create the list to save calculations to memory
 
-def application(): #programme in a function so it can be run indefinatly
-    opperation = input("What calculation would you like to perform? ") #ask for the opperation
+def selectOpperation(): #function to select opperation
+    opperation = input("What calculation would you like to perform? Type '?' for help: ") #ask for the opperation
+    #Checks for a valid opperation
+    if opperation == "+":
+        value2 = int(input("Enter the second number: ")) #ask for the second number
+    elif opperation == "-":
+        value2 = int(input("Enter the second number: ")) #ask for the second number
+    elif opperation == "*":
+        value2 = int(input("Enter the second number: ")) #ask for the second number
+    elif opperation == "/":
+        value2 = int(input("Enter the second number: ")) #ask for the second number
+    else:
+        print("The opperation you selected is not valid. Type ? for help")
+        selectOpperation()
+    return opperation
+
+def collectCalculation(): #function to collect the calculation from the user
     value1 = int(input("Enter the first number: ")) #ask for the first number
-    value2 = int(input("Enter the second number: ")) #ask for the second number
+    selectOpperation()
+    opperation = selectOpperation()
 
     #if statement to select the correct opperation depending on the given opperation
     if opperation == "+":
@@ -24,9 +40,9 @@ def application(): #programme in a function so it can be run indefinatly
     restart = input("Would you like to use the calculator again? ") #asks if they would like to use it again
 
     if restart == "y": #either exits the app or runs the function again
-        application()
+        collectCalculation()
     elif restart == "n":
         print("Thankyou for using my calculator!")
 
 
-application() #runs the function for the first time
+collectCalculation() #runs the function for the first time
